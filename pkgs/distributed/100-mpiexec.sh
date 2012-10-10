977dfd69c843c8da00a9edb3b14f9e50521f0c1e
voidcycles <derek.gerstmann@gmail.com>
2012-10-10 10:09:54 +0800

    Add HWLOC-GL for EQ.

--- pkgs/distributed/100-mpiexec.sh
+++ pkgs/distributed/100-mpiexec.sh
@@ -10,67 +10,59 @@ source "bldr.sh"
 # setup pkg definition and resource files
 ####################################################################################################
 
-pkg_ctry="graphics"
-pkg_name="eq"
+pkg_ctry="distributed"
+pkg_name="mpiexec"
 
-pkg_default="1.4"
-pkg_variants=("1.4")
-pkg_mirrors=("https://github.com/Eyescale/Equalizer/tarball/1.4")
+pkg_default="0.84"
+pkg_variants=("0.84")
 
-pkg_info="Equalizer is a middleware library used to create and deploy parallel OpenGL-based applications."
+pkg_info="ADLB is a software library designed to help rapidly build scalable parallel programs."
 
-pkg_desc="Equalizer is a middleware library used to create and deploy parallel OpenGL-based applications. 
+pkg_desc="ADLB is a software library designed to help rapidly build scalable parallel programs. 
 
-It enables applications to benefit from multiple graphics cards, processors and computers to scale 
-the rendering performance, visual quality and display size. An Equalizer application runs unmodified 
-on any visualization system, from a simple workstation to large scale graphics clusters, 
-multi-GPU workstations and Virtual Reality installations"
+ADLB is a software library designed to help rapidly build scalable parallel programs. 
+The name (pronounced adlib) is the acronym for Asynchronous Dynamic Load Balancing. 
 
-pkg_opts="cmake force-inplace-build"
-pkg_reqs="zlib vmm lunchbox glew "
-if [[ $BLDR_SYSTEM_IS_LINUX == true ]]; then
-    $pkg_reqs+="hwloc-gl "
-fi
-pkg_uses="$pkg_reqs"
+However, ADLB does not achieve scalability solely by load balancing. It also includes some 
+features that exploit work-stealing as well. Indeed, we sometimes use the phrase instantaneous 
+load balancing via work-stealing to describe ADLB. 
+
+ADLB will be available under the same license arrangement as MPICH2. 
+
+There are features of ADLB that are reminiscent of a variety of other systems, e.g. Linda. 
+However, ADLB is significantly different and unique in a number of ways."
+
+pkg_opts="configure"
+pkg_reqs="openmpi mvapich2 torque"
+pkg_uses="$pkg_uses"
 
 ####################################################################################################
 # satisfy pkg dependencies and load their environment settings
 ####################################################################################################
 
-bldr_satisfy_pkg               \
-  --category    "$pkg_ctry"    \
-  --name        "$pkg_name"    \
-  --version     "$pkg_default" \
-  --requires    "$pkg_reqs"    \
-  --uses        "$pkg_uses"    \
-  --options     "$pkg_opts"
+bldr_satisfy_pkg                    \
+    --category    "$pkg_ctry"       \
+    --name        "$pkg_name"       \
+    --version     "$pkg_default"    \
+    --requires    "$pkg_reqs"       \
+    --uses        "$pkg_uses"       \
+    --options     "$pkg_opts"
 
 ####################################################################################################
 
 pkg_cflags=""
 pkg_ldflags=""
-
-export BOOST_ROOT=$BLDR_BOOST_BASE_PATH
-export BOOST_INCLUDEDIR=$BLDR_BOOST_INCLUDE_PATH
-
-pkg_cfg="-DMAKESTATIC=1:-DLINKSTATIC=1"
-pkg_cfg+=":-DBOOST_ROOT=\"$BLDR_BOOST_BASE_PATH\""
-pkg_cfg+=":-DBOOST_INCLUDEDIR=\"$BLDR_BOOST_INCLUDE_PATH\""
-pkg_cfg+=":-DBoost_NO_SYSTEM_PATHS=ON"
-pkg_cfg+=":-DBoost_NO_BOOST_CMAKE=ON"
-pkg_cfg+=":-DBoost_DIR=\"$BLDR_BOOST_BASE_PATH\""
-pkg_cfg+=":-DBoost_INCLUDE_DIR=\"$BLDR_BOOST_INCLUDE_PATH\""
+pkg_cfg=""
 
 ####################################################################################################
 # register each pkg version with bldr
 ####################################################################################################
 
-let pkg_idx=0
 for pkg_vers in ${pkg_variants[@]}
 do
-    pkg_file="$pkg_name-$pkg_vers.tar.gz"
-    pkg_urls=${pkg_mirrors[$pkg_idx]}
-    
+    pkg_file="$pkg_name-$pkg_vers.tgz"
+    pkg_urls="https://www.osc.edu/~djohnson/mpiexec/$pkg_file"
+
     bldr_register_pkg                 \
          --category    "$pkg_ctry"    \
          --name        "$pkg_name"    \
@@ -87,8 +79,7 @@ do
          --ldflags     "$pkg_ldflags" \
          --config      "$pkg_cfg"     \
          --config-path "$pkg_cfg_path"
-
-    let pkg_idx++
 done
 
 ####################################################################################################
+
